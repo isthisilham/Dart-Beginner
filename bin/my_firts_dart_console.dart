224133ee7e13cd330230a2225ec7c3227ad279ed
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-01-22 11:13:48 +0700

    IF & Switch Dart

---
+++ bin/my_firts_dart_console.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+main (List<String> arguments) {
+
+  // PERCABANGAN
+
+  // int number = int.tryParse(stdin.readLineSync());
+  // String output;
+
+  // Percabangan IF
+
+  // output = (number % 2 == 0) ? "genap" : "ganjil";
+  // print(output);
+
+  // print((number % 2 == 0) ? "GENAP" : "GANJIL");
+
+  // if (number > 0){
+  //   print("Positive Number");
+  // } else {
+  //   print("Negative Number");
+  // }
+
+
+  //Percabangan Switch
+
+  // switch(number) {
+  //   case 0:
+  //     print("bilangan Null");
+  //     break;
+  //   case 1:
+  //     print("Satu");
+  //     break;
+  //   case 2:
+  //     print("Dua");
+  //     break;
+  //   default:
+  //     print("Bilangan Tidak Dikenali");
+  // }
+
+
+
+}
